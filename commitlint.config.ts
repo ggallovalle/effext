1e9e76bcb1e85212631ed95992f48ae45a80b9e1
Gerson Gallo <ggallovalle@gmail.com>
2026-04-08 00:04:15 -0600

    feat: add commitlint plugin

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,12 +1,31 @@
-import type { UserConfig } from "@commitlint/types"
+import type { Plugin, UserConfig } from "@commitlint/types"
 import { RuleConfigSeverity } from "@commitlint/types"
+
+const ClickRefPlugin = {
+  rules: {
+    "click-ref-exists": async (commit, when, opts) => {
+      console.log({
+        commit,
+        when,
+        opts,
+      })
+      return [true, "The click link exists"]
+    },
+  },
+} satisfies Plugin
 
 const Configuration: UserConfig = {
   extends: ["@commitlint/config-conventional"],
-  parserPreset: "conventional-changelog-atom",
   formatter: "@commitlint/format",
+  plugins: [ClickRefPlugin],
   rules: {
-    "type-enum": [RuleConfigSeverity.Error, "always", ["foo"]],
+    // "type-enum": [RuleConfigSeverity.Error, "always", ["foo"]],
+    "click-ref-exists": [
+      RuleConfigSeverity.Error,
+      "always",
+      // "never", // ok if true
+      { apiKey: "someSecret" },
+    ],
   },
   // ...
 }
