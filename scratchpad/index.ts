5311008d99d5f31ba013e06b2860121adb777fcb
Gerson Gallo <ggallovalle@gmail.com>
2026-04-10 02:32:27 -0600

    chore: update effect dependencies to latest
    ServiceMap is Context again - do not care
    Schema.makeUnsafe is make - nice

--- scratchpad/index.ts
+++ scratchpad/index.ts
@@ -4,7 +4,7 @@ import { Console, Effect } from "effect"
 const program = Effect.gen(function* () {
   yield* Console.log("hello")
 
-  const red = Uwu.Color.makeUnsafe({ format: "css", value: "red" })
+  const red = Uwu.Color.make({ format: "css", value: "red" })
   yield* Console.log("red")
   yield* Console.log(red)
 })
